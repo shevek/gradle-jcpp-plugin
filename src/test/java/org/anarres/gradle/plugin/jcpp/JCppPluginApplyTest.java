9f010e7900f1e1d6f8039fabea453bca123f99b2
Shevek <shevek@anarres.org>
2017-03-23 18:17:23 -0700

    Update to gradle 3.4.1; test with multiple versions, and use new plugin name.

--- src/test/java/org/anarres/gradle/plugin/jcpp/JCppPluginApplyTest.java
+++ src/test/java/org/anarres/gradle/plugin/jcpp/JCppPluginApplyTest.java
@@ -1,35 +1,75 @@
 package org.anarres.gradle.plugin.jcpp;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.util.Arrays;
 import java.util.Collections;
-import org.gradle.api.DefaultTask;
-import org.gradle.api.Project;
-import org.gradle.api.Task;
-import org.gradle.testfixtures.ProjectBuilder;
+import java.util.List;
+import javax.annotation.Nonnull;
+import org.gradle.testkit.runner.BuildResult;
+import org.gradle.testkit.runner.GradleRunner;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author shevek
  */
+@RunWith(Parameterized.class)
 public class JCppPluginApplyTest {
 
-    Project project;
+    private static final Logger LOG = LoggerFactory.getLogger(JCppPluginApplyTest.class);
+
+    @Nonnull
+    private static Object[] A(Object... in) {
+        return in;
+    }
+
+    @Parameterized.Parameters(name = "{0}")
+    public static List<Object[]> parameters() throws Exception {
+        return Arrays.asList(
+                A("2.12"),
+                A("2.14"),
+                A("3.0"),
+                A("3.2.1"),
+                A("3.4.1")
+        );
+    }
+
+    private final String gradleVersion;
+    @Rule
+    public final TemporaryFolder testProjectDir = new TemporaryFolder();
+    public File testProjectBuildFile;
 
     @Before
-    public void setUp() {
-        project = ProjectBuilder.builder().build();
+    public void setUp() throws Exception {
+        testProjectBuildFile = testProjectDir.newFile("build.gradle");
+    }
+
+    public JCppPluginApplyTest(String gradleVersion) {
+        this.gradleVersion = gradleVersion;
     }
 
     @Test
-    public void testApply() {
-        project.apply(Collections.singletonMap("plugin", "java"));
-        project.apply(Collections.singletonMap("plugin", "jcpp"));
-        assertTrue("Project is missing plugin", project.getPlugins().hasPlugin(JCppPlugin.class));
-        Task task = project.getTasks().findByName("jcpp");
-        assertNotNull("Project is missing jcpp task", task);
-        assertTrue("JCpp task is the wrong type", task instanceof DefaultTask);
-        assertTrue("JCpp task should be enabled", ((DefaultTask) task).isEnabled());
+    public void testApply() throws Exception {
+        String text = "plugins { id 'java';\nid 'org.anarres.jcpp' }\n";
+        Files.write(testProjectBuildFile.toPath(), Collections.singletonList(text));
+
+        GradleRunner runner = GradleRunner.create()
+                .withGradleVersion(gradleVersion)
+                .withPluginClasspath()
+                .withDebug(true)
+                .withProjectDir(testProjectDir.getRoot())
+                .withArguments("--stacktrace", "tasks");
+        LOG.info("Building...\n\n");
+        // System.out.println("ClassPath is " + runner.getPluginClasspath());
+        BuildResult result = runner.build();
+        LOG.info("Output:\n\n" + result.getOutput() + "\n\n");
     }
 }
